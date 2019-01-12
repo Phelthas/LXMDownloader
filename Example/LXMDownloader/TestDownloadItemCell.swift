181d1c89ac8a0409a84385d2bf168512fa5b12c5
Phelthas <billthas@gmail.com>
2019-01-12 11:51:09 +0800

    更新demo，修复可能出现的kvo没有及时移除的问题

--- Example/LXMDownloader/TestDownloadItemCell.swift
+++ Example/LXMDownloader/TestDownloadItemCell.swift
@@ -45,20 +45,20 @@ class TestDownloadItemCell: UITableViewCell {
     var observation: NSKeyValueObservation?
     
     var videoModel = TestVideoModel() {
+        willSet {
+            self.observation = nil //注意这里的写法，必须在setVideoModel之前把observation移除掉，随时系统会自己移除observer，但如果移除的不及时，就可能会出现对象还在KVO中就释放的bug。自己移除比较保险
+        }
         didSet {
             self.titleLabel.text = "\(videoModel.videoId)"
             self.statusLabel.text = "status \(videoModel.lxm_downloadItem?.downloadStatus ?? .none)"
             self.updateProgress()
             
             if videoModel.lxm_downloadItem != nil {
-                self.observation = self.videoModel.observe(\.lxm_downloadItem?.completedUnitCount) { [weak self] (model, change) in
+                self.observation = videoModel.observe(\.lxm_downloadItem?.completedUnitCount, options: [.new], changeHandler: { [weak self] (model, change) in
                     DispatchQueue.main.async {
                         self?.updateProgress()
-                        
-//                        print("\(change)")
                     }
-                    
-                }
+                })
             }
         }
     }
@@ -67,6 +67,8 @@ class TestDownloadItemCell: UITableViewCell {
         if let progress = self.videoModel.lxm_downloadItem?.progress {
             self.sizeLabel.text = "\(Int(progress * 100))%"
             self.progressView.progress = progress
+        } else {
+            self.progressView.progress = 0
         }
     }
     
